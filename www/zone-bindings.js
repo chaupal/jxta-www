a77693e5e4c3ca88622eb2a9fe76aef3176ee971
Henry Jen <slowhog@dev.java.net>
2007-06-20 04:28:24 +0000

    zone file used by implementations

---
+++ www/zone-bindings.js
@@ -0,0 +1,14 @@
+var zones = [
+    {
+        title: "JXTA Java SE",
+        href:  "https://jxta-jxse.dev.java.net/"
+    },
+    {
+        title: "JXTA C/C++/C#",
+        href:  "https://jxta-c.dev.java.net/"
+    },
+    {
+        title: "JXTA Java ME",
+        href:  "https://jxta-jxme.dev.java.net/"
+    }
+];
